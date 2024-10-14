2f8d3ac9c58dab0610c52e692e04ed0777e550c4
camfung <cameronfuung@gmail.com>
2024-10-13 22:30:44 -0700

    added tests for send file invalid extension

--- client/src/tests/sendFile.test.js
+++ client/src/tests/sendFile.test.js
@@ -25,4 +25,31 @@ describe("fetchFileTgext", () => {
 		const formData = axios.post.mock.calls[0][1];
 		expect(formData.get("File")).toEqual(mockFile);
 	});
-}); 
+});
+
+describe("FetchFileTextWrongExt", () => {
+	it("should send a file that doesnt have a .txt or .pdf ext and expect a 400 error message", async () => {
+		const mockFile = new File(["file content"], "test.fail", { type: "text/plain" });
+		const mockError = {
+			response: {
+				status: 400,
+				data: { error: "multer Error: Invalid file extension" },
+			}
+		}
+
+		axios.post.mockRejectedValueOnce(mockError);
+
+		try {
+			await fetchFileText(mockFile);
+		} catch (error) {
+			expect(axios.post).toHaveBeenCalledWith("/single", expect.any(FormData));
+			const formData = axios.post.mock.calls[0][1];
+			expect(formData.get("File")).toEqual(mockFile);
+
+			expect(error.response.status).toBe(400);
+			expect(error.response.data.error).toBe("multer Error: Invalid file extension")
+		}
+
+
+	})
+})
